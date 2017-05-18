1e4282e53a718113122b8a5c5f88f0e89b945bac
samvel.kocharyan <hedgehog.millenium@gmail.com>
2017-05-18 16:37:33 +0400

    Callback functionality added

--- scraper.py
+++ scraper.py
@@ -1,77 +1,123 @@
-from urllib import request, error, parse ,robotparser
-import datetime , time , re, random as rnd
+import re
+import time
+import random
+import queue
+
+from urllib import request, error, parse, robotparser
+from datetime import datetime
+
+user_agents = [
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.96 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:47.0) Gecko/20100101 Firefox/47.0',
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X x.y; rv:42.0) Gecko/20100101 Firefox/42.0',
+    'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36',
+]
 
 
-def download(url, user_agent=None,proxy=None , num_retries=2):
-    user_agents = [
-        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.96 Safari/537.36',
-        'Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:47.0) Gecko/20100101 Firefox/47.0',
-        'Mozilla/5.0 (Macintosh; Intel Mac OS X x.y; rv:42.0) Gecko/20100101 Firefox/42.0',
-        'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36',
-    ]
-
-    print('Downloading ', url)
-    headers = {'User-agent': user_agent if user_agent else rnd.choice(user_agents)}
-    req = request.Request(url, headers=headers)
-    opener = request.build_opener()
-    if proxy:
-        proxy_params = {parse.urlparse(url).scheme: proxy}
-    opener.add_handler(request.ProxyHandler(proxy_params))
-    try:
-        html = request.urlopen(req).read()
-    except error.HTTPError as e:
-        print('Download error:', e.reason)
-        if num_retries > 0:
-            if hasattr(e, 'code') and 500 <= e.code < 600:
-                return download(url, num_retries - 1)
-            html = None
-    return html
-
-
-def crawl_sitemap(url):
-    # download the sitemap file
-    sitemap = download(url)
-    # extract the sitemap links
-    links = re.findall('<loc>(.*?)</loc>', sitemap)
-    # download each link
-    for link in links:
-        html = download(link)
-
-
-def link_crawler(seed_url, link_regex=None, delay=5, max_depth=-1, max_urls=-1, headers=None, user_agent='wswp', proxy=None, num_retries=1):
-    crawl_queue = [seed_url]
+def link_crawler(seed_url, link_regex=None, delay=1, max_depth=-1, max_urls=-1, headers=None, user_agent=None,
+                 proxy=None, num_retries=1,scrape_callback=None):
+    """Crawl from the given seed URL following links matched by link_regex
+    """
+    # the queue of URL's that still need to be crawled
+    crawl_queue = queue.deque([seed_url])
+    # the URL's that have been seen and at what depth
     seen = {seed_url: 0}
+    # track how many URL's have been downloaded
+    num_urls = 0
     rp = get_robots(seed_url)
     throttle = Throttle(delay)
     headers = headers or {}
-    if user_agent:
-        headers['User-agent'] = user_agent
+    if not user_agent:
+        user_agent = random.choice(user_agents)
 
     while crawl_queue:
         url = crawl_queue.pop()
-
+        depth = seen[url]
+        # check url passes robots.txt restrictions
         if rp.can_fetch(user_agent, url):
             throttle.wait(url)
-            html = download(url)
+            html = download(url, headers, proxy=proxy, num_retries=num_retries)
             links = []
 
-            depth = seen[url]
+            if scrape_callback:
+                links.extend(scrape_callback(url, html) or [])
+
             if depth != max_depth:
+                # can still crawl further
                 if link_regex:
+                    # filter for links matching our regular expression
                     links.extend(link for link in get_links(html) if re.match(link_regex, link))
 
-                for link in link:
+                for link in links:
                     link = normalize(seed_url, link)
+                    # check whether already crawled this link
                     if link not in seen:
+                        seen[link] = depth + 1
+                        # check link is within same domain
                         if same_domain(seed_url, link):
-                            seen[link] = depth+1
+                            # success! add this new link to queue
                             crawl_queue.append(link)
+
+            # check whether have reached downloaded maximum
+            num_urls += 1
+            if num_urls == max_urls:
+                break
+        else:
+            print('Blocked by robots.txt:', url)
+
+
+class Throttle:
+    """Throttle downloading by sleeping between requests to same domain
+    """
+
+    def __init__(self, delay):
+        # amount of delay between downloads for each domain
+        self.delay = delay
+        # timestamp of when a domain was last accessed
+        self.domains = {}
+
+    def wait(self, url):
+        domain = parse.urlparse(url).netloc
+        last_accessed = self.domains.get(domain)
+
+        if self.delay > 0 and last_accessed is not None:
+            sleep_secs = self.delay - (datetime.now() - last_accessed).seconds
+            if sleep_secs > 0:
+                time.sleep(sleep_secs)
+        self.domains[domain] = datetime.now()
+
+
+def download(url, headers, proxy, num_retries, data=None):
+    print('Downloading:', url)
+    if 'User-agent' not in headers.keys():
+        headers['User-agent'] = random.choice(user_agents)
+
+    req = request.Request(url, data, headers)
+    opener = request.build_opener()
+    if proxy:
+        proxy_params = {parse.urlparse(url).scheme: proxy}
+        opener.add_handler(parse.ProxyHandler(proxy_params))
+    try:
+        response = opener.open(req)
+        html = response.read()
+        # code = response.code
+    except error.URLError as e:
+        print('Download error:', e.reason)
+        html = ''
+        if hasattr(e, 'code'):
+            code = e.code
+            if num_retries > 0 and 500 <= code < 600:
+                # retry 5XX HTTP errors
+                return download(url, headers, proxy, num_retries - 1, data)
+        else:
+            code = None
+    return html
 
 
 def normalize(seed_url, link):
     """Normalize this URL by removing hash and adding domain
     """
-    link, _ = parse.urldefrag(link) # remove hash to avoid duplicates
+    link, _ = parse.urldefrag(link)  # remove hash to avoid duplicates
     return parse.urljoin(seed_url, link)
 
 
@@ -91,23 +137,17 @@ def get_robots(url):
 
 
 def get_links(html):
+    """Return a list of links from html 
+    """
+    # a regular expression to extract all links from the webpage
     webpage_regex = re.compile('<a[^>]+href=["\'](.*?)["\']', re.IGNORECASE)
-    return webpage_regex.findall(html)
+    # list of all links from the webpage
+    st = str(html)
+
+    return webpage_regex.findall(st)
 
 
-class Throttle:
-    def __init__(self,delay):
-        self.delay = delay
-        self.domains = {}
-
-    def wait(self,url):
-        domain = parse.urlparse(url).netloc
-        last_accessed = self.domains.get(domain)
-
-        if self.delay>0 and last_accessed is not None:
-            sleep_secs = self.delay - (datetime.datetime.now()-last_accessed).seconds
-            if sleep_secs > 0:
-                time.sleep(sleep_secs) \
- \
-        self.domains[domain] = datetime.datetime.now()
-
+if __name__ == '__main__':
+    link_crawler('http://example.webscraping.com', '/(index|view)', delay=0, num_retries=1, user_agent='BadCrawler')
+    link_crawler('http://example.webscraping.com', '/(index|view)', delay=0, num_retries=1, max_depth=1,
+                 user_agent='GoodCrawler')
--- start.py
+++ start.py
@@ -1,6 +1,34 @@
 import scraper
+import re, lxml, lxml.html
+import csv
 
-url = 'https://metanit.com/python/tutorial/8.2.php'
-sitemap_url = 'https://metanit.com/sitemap.xml'
-html = scraper.download(url,user_agent='BadCrawler')
-print(html)
+
+# url = 'https://metanit.com/python/tutorial/8.2.php'
+# sitemap_url = 'https://metanit.com/sitemap.xml'
+
+class ScrapeCallback:
+    def __init__(self):
+        self.writer = csv.writer(open('countries.csv', 'w'))
+        self.fields = ('area', 'population', 'iso', 'country',
+                       'capital', 'continent', 'tld', 'currency_code',
+                       'currency_name', 'phone', 'postal_code_format',
+                       'postal_code_regex', 'languages',
+                       'neighbours')
+        self.writer.writerow(self.fields)
+
+    def __call__(self, url, html):
+        if re.search('/view/', url):
+            tree = lxml.html.fromstring(html)
+            row = []
+            for field in self.fields:
+                row.append(tree.cssselect('table > tr#places_{}__row > td.w2p_fw'.format(field))[0].text_content())
+                self.writer.writerow(row)
+
+
+seed_url = 'http://example.webscraping.com/index'
+link_regex = '/(index|view)'
+links = scraper.link_crawler(seed_url, link_regex, max_depth=1, scrape_callback=ScrapeCallback())
+
+print(links)
+
+
