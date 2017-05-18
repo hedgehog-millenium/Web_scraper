3d7de4c6eff548bd0988ba4dc87612a840fa5133
samvel.kocharyan <hedgehog.millenium@gmail.com>
2017-05-18 17:32:02 +0400

    Cache implementation started

--- start.py
+++ start.py
@@ -9,20 +9,14 @@ import csv
 class ScrapeCallback:
     def __init__(self):
         self.writer = csv.writer(open('countries.csv', 'w'))
-        self.fields = ('area', 'population', 'iso', 'country',
-                       'capital', 'continent', 'tld', 'currency_code',
-                       'currency_name', 'phone', 'postal_code_format',
-                       'postal_code_regex', 'languages',
-                       'neighbours')
-        self.writer.writerow(self.fields)
 
     def __call__(self, url, html):
         if re.search('/view/', url):
-            tree = lxml.html.fromstring(html)
             row = []
-            for field in self.fields:
-                row.append(tree.cssselect('table > tr#places_{}__row > td.w2p_fw'.format(field))[0].text_content())
-                self.writer.writerow(row)
+            html_rows = lxml.html.fromstring(html).cssselect('tr > td.w2p_fw')
+            for r in html_rows :
+                row.append(r.text_content())
+            self.writer.writerow(row)
 
 
 seed_url = 'http://example.webscraping.com/index'
